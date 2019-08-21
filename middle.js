d91d6bab833ce64dcddf84f03e0964f41a1d293c
vagrant <Kennethzhang374@gmail.com>
2019-08-21 06:20:44 +0000

    Middle got it at home in 15 mins, bravo

---
+++ middle.js
@@ -0,0 +1,17 @@
+const middle = function (arr) {
+  let newArr = [];
+  if (arr.length <= 2) {
+    return newArr;
+  } else if (arr.length > 2 && arr.length % 2 === 0) {
+    let middleIndex = arr.length / 2;
+    let answer = arr.slice(middleIndex - 1, middleIndex + 1);
+    return answer;
+  } else if (arr.length > 2 && arr.length % 2 !== 0) {
+    let middleIndex = arr.length / 2;
+    let middleInt = Math.floor(middleIndex);
+    let answer = arr.slice(middleInt, middleInt + 1);
+    return answer;
+  } 
+};
+
+console.log(middle([1,2]));
