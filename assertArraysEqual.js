a4dd43207d2f6e5bb87e3a3d9b5c287dc39e5f2a
vagrant <Kennethzhang374@gmail.com>
2019-08-26 19:28:43 +0000

    refactored

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,21 +1,15 @@
-const eqArrays = function(Arr1, Arr2) {
-  
-  for (let i = 0; i <= Arr1.length -1; i++)
-  if (Arr1[i] !== Arr2[i]) {
-    return false;
-  } else {}
-  return true;
-};
 
-const assertArraysEqual = function(arr1, arr2) {
-  if (eqArrays(arr1,arr2) === true) {
-    return `✅✅✅ Assertion Passed ${arr1} is identical to ${arr2}`
-  } else {
-    return `🛑🛑🛑 Assertion Failed ${arr1} is Not identical to ${arr2}`
-  }
-};
+const eqArrays = require('./eqArrays');
 
-console.log(assertArraysEqual([1, 2, 3], [1, 2, 3]));
-console.log(assertArraysEqual([1, 2, 3], [3, 2, 1]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]));
-console.log(assertArraysEqual(["1", "2", "3"], ["1", "2", 3]));
+  const assertArraysEqual = function(arr1, arr2) {
+    if (eqArrays(arr1,arr2) === true) {
+      return `✅✅✅ Assertion Passed ${arr1} is identical to ${arr2}`
+    } else {
+      return `🛑🛑🛑 Assertion Failed ${arr1} is Not identical to ${arr2}`
+    }
+  };
+ 
+
+module.exports = assertArraysEqual;
+
+
