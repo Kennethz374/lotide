bac2016e731e8a65267e81f30f4bf76b8fa02e05
vagrant <Kennethzhang374@gmail.com>
2019-08-26 16:15:08 +0000

    refactored

--- tail.js
+++ tail.js
@@ -3,26 +3,5 @@ const tail = function(data) {
   return data;
 };
 
+module.exports = tail;
 
-// FUNCTION IMPLEMENTATION
-const assertEqual = function(actual, expected) {
-  if (actual.toString() === expected.toString()) {
-    return `\uD83D\uDE00 \uD83D\uDE00 Assertion Passed ${actual} equal ${expected}`;
-  } else {
-    return `\uD83D\uDE00 \uD83D\uDE00 Assertion failed ${actual} Not equal ${expected}`;
-  }
-};
-
-// TEST CODE//
-
-console.log(assertEqual("Lighthouse Labs", "Bootcamp"));
-console.log(assertEqual("w", "w"));
-
-console.log(assertEqual(tail([1,4]), 5));
-console.log(assertEqual(tail(["I", "Hello", "do"]), ["Hello", "do"]));
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-console.log(assertEqual(words.length, 3));
-
-///data length fail come back ///
